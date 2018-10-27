b353df1bcc84bf07e9b948e13f6ddaa9737e5f5d
stephencordasco <stephencordasco@gmail.com>
2018-10-26 17:10:09 -0700

    Added functionality to the seek bar. Added a method to calculate the tip. (Needs logical fix)

--- app/src/main/java/com/example/steph/multicalculator/TipCalculatorActivity.kt
+++ app/src/main/java/com/example/steph/multicalculator/TipCalculatorActivity.kt
@@ -4,8 +4,12 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.SeekBar
 import kotlinx.android.synthetic.main.activity_tip_calculator.*
+import java.lang.StringBuilder
+import java.text.DecimalFormat
 
 class TipCalculatorActivity : AppCompatActivity() {
+
+    var progressValue = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,7 +17,7 @@ class TipCalculatorActivity : AppCompatActivity() {
 
         seekBar?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-
+                progressValue = progress
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {
@@ -21,8 +25,40 @@ class TipCalculatorActivity : AppCompatActivity() {
             }
 
             override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                // build a string of the tip to print to the application
+                val tip = StringBuilder()
+                tip.append("%")
+                tip.append(progressValue.toString())
+                // set the text of the textview
+                tipTextView2.text = tip
 
+                // calculate after the seek bar stops tracking
+                calculateTip()
             }
         })
+    }
+
+    fun calculateTip() {
+        // check if the edittext field is empty
+        if (priceEditText.text.toString() == "") {
+            priceEditText.setText("0")
+        }
+
+        // used to format the decimal
+        val precision = DecimalFormat("0.00")
+
+        // store the price
+        val price = java.lang.Double.parseDouble(priceEditText.text.toString())
+        // calculate and store the tip
+        val tip: Double = price * (progressValue/100)
+        // calculate and store the total
+        val total: Double = price + tip
+
+        // build a string of the total to print to the application
+        val totalValue = StringBuilder()
+        totalValue.append("$")
+        totalValue.append(precision.format(total).toString())
+        // set the text of the textview
+        totalTextView2.text = totalValue
     }
 }
